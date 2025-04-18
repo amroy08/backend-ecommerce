176a6c00384351822c089500bbdd5e51c67fed28
amroy08 <166955301+amroy08@users.noreply.github.com>
2025-04-18 21:52:24 +0530

    Update categoryModel.js
    qwred

--- models/categoryModel.js
+++ models/categoryModel.js
@@ -1,13 +1,17 @@
 import mongoose from "mongoose";
 
-const categorySchema = new mongoose.Schema({
-  name: {
-    type: String,
-    trim: true,
-    required: true,
-    maxLength: 32,
-    unique: true,
+const categorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
   },
-});
+  { timestamps: true }
+);
 
-export default mongoose.model("Category", categorySchema);
+const Category = mongoose.model("Category", categorySchema);
+
+export default Category;
